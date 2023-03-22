af3954c4412f0ee62081ee1d4ff67d8a88cf7b85
范力 <798347235@qq.com>
2023-03-22 16:32:35 +0800

    feat: 删除不需要的引用

--- src/pages/scene/index.tsx
+++ src/pages/scene/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useMemo, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { useTweakpane } from '@/hooks/useTweakpane';
 
